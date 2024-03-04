133cc39af6fbb014b945648e73e1968d24fbbab7
Eoghan Walsh <eoghanpwalsh@gmail.com>
2024-03-04 21:55:34 +0000

    weekly task 06

---
+++ week06/square_root.py
@@ -0,0 +1,20 @@
+# square_root.py
+
+# program that takes a positive floating-point number as
+#   an input and outputs an approximation of its square root
+#   using "Newton's" method to compute square roots.
+
+# ref1: https://www.youtube.com/watch?v=FpOEx6zFf1o
+# ref2: https://www.w3schools.com/python/python_functions.asp
+
+# author: eoghan walsh
+
+# square root function
+def sqrt():
+    number = float(input("Please enter a positive number: "))
+    approx_sqrt = 1
+    for x in range(10):
+        approx_sqrt = (((number / approx_sqrt) + approx_sqrt) / 2)
+    print(f"The square root of {number} is approx. {approx_sqrt}")
+
+sqrt()
