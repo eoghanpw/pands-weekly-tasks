e5aa9839ac89c3c5beac42426c4c934675a0cd54
Eoghan Walsh <eoghanpwalsh@gmail.com>
2024-04-06 12:27:26 +0100

    weekly task 2 update

--- week02/README.md
+++ week02/README.md
@@ -1,1 +1,8 @@
-# week02
+# Weekly Task 2: bank.py
+
+This program prompts the user and reads in two money amounts (in cent),
+and then prints out the sum of the two numbers in euro and cent.
+
+## References
+
+https://www.w3schools.com/python/ref_string_format.asp
--- week02/bank.py
+++ week02/bank.py
@@ -1,10 +1,17 @@
-# bank.py
+# Weekly Task 2: bank.py
 # This program prompts the user and reads in two money amounts (in cent),
-# then adds the two amounts and prints out the answer in readable format with Euro sign and decimal point.
+# then adds the two amounts and prints out the answer
+# in readable format with Euro sign and decimal point.
 # Author: Eoghan Walsh
 
+# Prompt user to input two amounts.
 amount1 = int(input('Enter amount1(in cent): '))
 amount2 = int(input('Enter amount2(in cent): '))
-amount3 = (amount1 + amount2) / 100
-amount4 = "{:,.2f}".format(amount3) # formats number to 2 decimal places and adds thousands separator. Reference: https://pythonguides.com/python-format-number-with-commas/
-print(f'The sum of these is €{amount4}')
+
+# Sum the amounts and convert to euro and cent.
+sum = (amount1 + amount2) / 100
+
+# Format the number to 2 decimal places and add thousand separator.
+formatted_sum = "{:,.2f}".format(sum)
+
+print(f'The sum of these is €{formatted_sum}')
