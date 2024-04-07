dfcdd1cdfbeacfa4e8186346d8239600072de92f
Eoghan Walsh <eoghanpwalsh@gmail.com>
2024-04-07 21:33:32 +0100

    weekly task 7 update

---
+++ week07/README.md
@@ -0,0 +1,10 @@
+# Weekly Task 7: es.py
+
+Program that reads in a text file from the command line and outputs the number of e's it contains.
+
+## References
+
+https://pythonexamples.org/python-count-occurrences-of-word-in-text-file/
+https://docs.python.org/3/library/sys.html
+https://www.w3schools.com/python/python_file_handling.asp
+https://www.w3schools.com/python/python_try_except.asp
---
+++ week07/es.py
@@ -0,0 +1,33 @@
+# Weekly Task 7: es.py
+# Program that reads in a text file and outputs
+# the number of e's it contains.
+# Author: Eoghan Walsh
+
+# Import sys.
+import sys
+
+# Import os.path.
+import os.path
+
+# Filename can be input on command line.
+try:
+    FILENAME = sys.argv[1]
+except:
+    print("Please enter filename.")
+    sys.exit(1)
+
+
+# Function to count the occurances of the letter e in the file.
+def e_count():
+    with open(FILENAME) as f:
+        str = f.read()
+        count = str.count("e") + str.count("E")
+        print(count)
+
+
+if not os.path.isfile(FILENAME):
+    print("File does not exist.")
+elif ".txt" not in FILENAME:
+    print("File extension not valid. Please select .txt file.")
+else:
+    e_count()
