3bfe91d1a9338919ac3f0ff3a095d03ec99abf24
Eoghan Walsh <eoghanpwalsh@gmail.com>
2024-04-07 17:51:23 +0100

    week05 task updates

---
+++ week05/README.md
@@ -0,0 +1,16 @@
+# Weekly Task 5: weekday.py
+
+Program that outputs the whether or not today is a weekday.
+
+If today is a Saturday or Sunday the program outputs:
+
+    Yay, it's the weekend!
+
+Otherwise the program outputs:
+
+    Unfortunately today is a weekday. Boo!
+
+
+## References
+
+https://www.w3schools.com/python/python_datetime.asp
--- week05/weekday.py
+++ week05/weekday.py
@@ -1,15 +1,20 @@
-# weekday
-# program that outputs whether or not today is a weekday
-# author: eoghan walsh
+# Weekly Task 5: weekday.py
+# Program that outputs whether or not today is the weekend.
+# Author: Eoghan Walsh
 # ref: https://www.w3schools.com/python/python_datetime.asp
 
+# Import the datetime module.
 import datetime
 
+# Get the current date.
 today = datetime.datetime.today()
 
-if today.strftime("%A") == "Saturday" or "Sunday":
+# Format date to weekday string.
+weekday = today.strftime("%A")
+
+# Print response based on weekday.
+if weekday == "Saturday" or "Sunday":
     print("Yay, it's the weekend!")
 
 else:
     print("Unfortunately today is a weekday. Boo!")
-
